36e3e5afe9f33699a9ae194c0cbf5e675dabaa4e
Lorenzo Faivre <lfaivre@asu.edu>
2020-06-24 15:43:03 -0700

    feat: create models

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-use-before-define */
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const {
   GraphQLObjectType,
@@ -10,23 +12,6 @@ const {
   GraphQLList,
   GraphQLSchema,
 } = graphql;
-
-// TEMP :: Placeholder Data
-
-const books = [
-  { name: 'Book One', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'Book Two', genre: 'Fantasy', id: '2', authorId: '2' },
-  { name: 'Book Three', genre: 'Sci-Fi', id: '3', authorId: '3' },
-  { name: 'Book Four', genre: 'Fantasy', id: '1', authorId: '2' },
-  { name: 'Book Five', genre: 'Fantasy', id: '2', authorId: '3' },
-  { name: 'Book Six', genre: 'Fantasy', id: '3', authorId: '3' },
-];
-
-const authors = [
-  { name: 'Author One', age: 20, id: '1' },
-  { name: 'Author Two', age: 30, id: '2' },
-  { name: 'Author Three', age: 40, id: '3' },
-];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -38,7 +23,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return _.find(authors, { id: parent.authorId });
+        // return _.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -54,7 +39,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        // return _.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -66,29 +51,31 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: { id: { type: GraphQLID } },
+      // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
+      // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        // return _.find(authors, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return books;
+        // return books;
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        return authors;
+        // return authors;
       },
     },
   },
