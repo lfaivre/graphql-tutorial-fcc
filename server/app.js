5c244b12fd5ff207f9951929c8b504a630456ec1
Lorenzo Faivre <lfaivre@asu.edu>
2020-06-24 11:25:53 -0700

    feat: basic graphql setup with initial schema

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
