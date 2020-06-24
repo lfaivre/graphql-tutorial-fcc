4a5f6ae246e387ed5c1f711a3f534dc16364ef9b
Lorenzo Faivre <lfaivre@asu.edu>
2020-06-24 15:32:29 -0700

    feat: initial mongodb setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
+/* eslint-disable no-console */
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema');
 
 const app = express();
+
+const DB_URI = `mongodb+srv://${process.env.MONGODB_USER_ADMIN_USERNAME}:${process.env.MONGODB_USER_ADMIN_PASSWORD}@general-91kb5.mongodb.net/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).catch((error) => {
+  throw new Error(`Connection to MongoDB database failed.\n${error}`);
+});
+mongoose.connection.once('open', () => {
+  console.log('Connected to database.');
+});
 
 app.use(
   '/graphql',
@@ -14,6 +26,5 @@ app.use(
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}...`);
 });
