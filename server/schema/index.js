efe73bebefdf0c97b5e3e1c29ee95f3df57e9e78
Lorenzo Faivre <lfaivre@asu.edu>
2020-06-24 16:09:59 -0700

    feat: complete mongodb and graphql connection

--- server/schema/index.js
+++ server/schema/index.js
@@ -23,7 +23,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -39,7 +39,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return _.filter(books, { authorId: parent.id });
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -53,7 +53,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
     author: {
@@ -61,21 +61,21 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
     books: {
       type: new GraphQLList(BookType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return books;
+        return Book.find({});
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       // eslint-disable-next-line no-unused-vars
       resolve(parent, args) {
-        // return authors;
+        return Author.find({});
       },
     },
   },
